e769d47ff13f219791f7062c441571e02ba2c226
junior <antonioivo.3@gmail.com>
2023-12-14 16:22:39 -0300

    first landpage

---
+++ src/views/Home/ContactPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-center w-full">
+    <div class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+      <h1 class="text-4xl font-black text-center text-gray-800">
+        Alguma duvida ?
+      </h1>
+      <p class="text-lo text-center text-gray-800 font-regular">Quer saber mais sobre os precos ?</p>
+      <div class="mt-10">
+        <a href="mailto:" class="px-6 py-2 mt-10 font-bold text-white rounded-full bg-brand-main focus:outline-none">
+          nos mande um e-email
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/Home/indexHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <CustomHeader @create-account="handleAccountCreate" @login="handleLogin" />
+  <ContactPage />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedback 2023</p>
+  </div>
+</template>
+
+<script>
+import CustomHeader from './CustomHeader.vue'
+import ContactPage from './ContactPage.vue'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  components: { CustomHeader, ContactPage },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
